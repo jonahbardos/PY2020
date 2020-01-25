1b34d2c152e50f1bbdcafc9339df78fea015885a
andrewbriand <atb8888@comcast.net>
2020-01-25 13:14:54 -0800

    Try to terminate on linter failure

--- tests-docker-action/entrypoint.sh
+++ tests-docker-action/entrypoint.sh
@@ -1,7 +1,7 @@
 echo "test"
 apt-get install -y clang-format
 git clone https://github.com/Sarcasm/run-clang-format.git
-./run-clang-format/run-clang-format.py -r ./src
+./run-clang-format/run-clang-format.py -r ./src || return 1
 git clone https://github.com/catchorg/Catch2.git
 cd ./Catch2
 cmake -B./build -H. -DBUILD_TESTING=OFF
