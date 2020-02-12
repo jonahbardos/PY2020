f03bc4bb59cf2450fee2b3543437a04bb57b2e70
Evan Straw <evan.straw99@gmail.com>
2020-02-11 19:22:47 -0800

    Add Zed camera params

--- src/ar/CameraParams.h
+++ src/ar/CameraParams.h
@@ -39,4 +39,22 @@ constexpr double laptop_dist[] = {3.4425270669197583e-01, -4.5627505509852968e+0
 								  1.5720190712074883e+01};
 const CameraParams LAPTOP_PARAMS(cv::Mat(3, 3, CV_64F, *laptop_params),
 								 cv::Mat(5, 1, CV_64F, *laptop_dist));
+
+constexpr double zed_left_params[] = {5.3884982625775467e+02, 0., 8.2217958600564066e+02,
+									  0.,5.4090851594064463e+02, 4.9235905127262106e+02,
+									  0., 0., 1.};
+constexpr double zed_left_dist[] = {4.7517264791230152e-01, -3.4786883943555763e-01,
+									0., 0., 0., 0., 0., -6.4469359027443565e-02, 0.,
+									0., 0., 0., 0., 0.};
+const CameraParams ZED_LEFT_PARAMS(cv::Mat(3, 3, CV_64F, *zed_left_params),
+								   cv::Mat(14, 1, CV_64F, *zed_left_dist));
+
+constexpr double zed_right_params[] = {5.3884982625775467e+02, 0., 1.1200738883335107e+03,
+									   0.,5.4090851594064463e+02, 5.0675271287599782e+02,
+									   0., 0., 1.};
+constexpr double zed_right_dist[] = {-2.6567023852688363e-03, 9.0322135223047925e-02, 0.,
+									 0., 0., 0., 0., 3.0786835002184900e-02, 0., 0., 0.,
+									 0., 0., 0.};
+const CameraParams ZED_RIGHT_PARAMS(cv::Mat(3, 3, CV_64F, *zed_right_params),
+									cv::Mat(14, 1, CV_64F, *zed_right_params));
 }
